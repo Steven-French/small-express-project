b5dffae36d17c1a05a5d4d1d7e831fcccdb260db
Steven French <steven.french97@yahoo.com>
2021-11-11 19:48:43 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server= http.createServer(app);
+
+app.use(express.static('public'));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrapp/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrapp/dist/js')));
+
+app.set('view engine', 'ejs');
+
+const routes = require('./routes');
+routes(app); routes
+
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+server.listen(port, () => {
+	console.log(`Listening on *.${port}...`);
+});
+
